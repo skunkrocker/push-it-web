2cb379d8b58a336f8d06a9a6063883ffeb551306
skunkrocker (•̀ᴗ•́)و <n.spasikov@gmail.com>
2020-11-10 09:48:03 +0100

    use the constants from the constant class fix controller tests add authentication manager test

--- src/main/kotlin/machinehead/pushitweb/security/config/UserAuthenticationManager.kt
+++ src/main/kotlin/machinehead/pushitweb/security/config/UserAuthenticationManager.kt
@@ -1,5 +1,8 @@
 package machinehead.pushitweb.security.config
 
+import machinehead.pushitweb.constants.Constants.Companion.TEST_PASSWORD
+import machinehead.pushitweb.constants.Constants.Companion.TEST_ROLE
+import machinehead.pushitweb.constants.Constants.Companion.TEST_USER
 import machinehead.pushitweb.repositories.PushUserRepository
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
@@ -7,11 +10,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 
 open class UserAuthenticationManager(private val pushUserRepository: PushUserRepository) : AuthenticationManager {
-    companion object {
-        const val TEST_USER = "test-user"
-        const val TEST_ROLE = "TEST_ADMIN"
-        const val TEST_PASSWORD = "test-password"
-    }
+
 
     override fun authenticate(authentication: Authentication?): Authentication? {
 
