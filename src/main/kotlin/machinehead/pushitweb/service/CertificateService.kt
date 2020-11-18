3cdc00098fe5ae14ee88b191601d463aa765fff1
skunkrocker (•̀ᴗ•́)و <n.spasikov@gmail.com>
2020-11-18 10:53:52 +0100

    controller advice exception handling in dem certificate service

--- src/main/kotlin/machinehead/pushitweb/service/CertificateService.kt
+++ src/main/kotlin/machinehead/pushitweb/service/CertificateService.kt
@@ -1,12 +1,14 @@
 package machinehead.pushitweb.service
 
-import machinehead.pushitweb.entities.Application
-import machinehead.pushitweb.repositories.ApplicationRepository
+import java.util.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
 import org.springframework.stereotype.Service
+import machinehead.pushitweb.entities.Application
 import org.springframework.web.multipart.MultipartFile
-import java.util.*
+import machinehead.pushitweb.repositories.ApplicationRepository
+import org.springframework.dao.DataIntegrityViolationException
 
 @Service
 class CertificateService(private val applicationRepository: ApplicationRepository) {
@@ -14,21 +16,45 @@ class CertificateService(private val applicationRepository: ApplicationRepositor
     private val LOGGER: Logger = LoggerFactory.getLogger(CertificateService::class.java)
 
     fun saveCertificate(appName: String, certificatePassword: String, certificateFile: MultipartFile): String {
-        if (appName.isBlank() or certificatePassword.isBlank() or certificateFile.isEmpty) {
-            LOGGER.error("Invalid certificate information.")
-            LOGGER.error("The appName: {}, password: {},  certificate file: {}", appName.isBlank(), certificatePassword.isBlank(), certificateFile.isEmpty)
 
-            //TODO throw exception
-        }
+        validateArguments(appName, certificatePassword, certificateFile)
 
         val encodedCertificate = Base64.getEncoder().encodeToString(certificateFile.bytes)
 
         LOGGER.debug("The certificate was encoded: {}", encodedCertificate.substring(0, 10))
 
-        applicationRepository.save(Application(appName, certificatePassword, encodedCertificate))
-
-        LOGGER.debug("The certificate with the name: {} was stored to the database.", appName)
+        saveApplication(appName, certificatePassword, encodedCertificate)
 
         return appName
+    }
+
+    private fun saveApplication(appName: String, certificatePassword: String, encodedCertificate: String) =
+            try {
+                applicationRepository.save(Application(appName, certificatePassword, encodedCertificate))
+                LOGGER.debug("The certificate with the name: {} was stored to the database.", appName)
+
+            } catch (e: DataIntegrityViolationException) {
+                val message = "The app with the name: %s already exists.".format(appName)
+                LOGGER.error(message, e)
+                throw IllegalArgumentException(message)
+            }
+
+    private fun validateArguments(appName: String, certificatePassword: String, certificateFile: MultipartFile) {
+
+        if (appName.isBlank() or certificatePassword.isBlank() or certificateFile.isEmpty) {
+            val message = "The appName is blank: %s, password is blank: %s,  certificate file is empty: %s".format(appName.isBlank(), certificatePassword.isBlank(), certificateFile.isEmpty)
+            LOGGER.error("Invalid certificate information.")
+            LOGGER.error(message)
+
+            throw IllegalArgumentException(message)
+        }
+
+        if (certificateFile.contentType != "application/x-pkcs12") {
+            val message = "The content type of the file is not supported: %s. At the moment only application/x-pkcs12 certificates are supported."
+                    .format(certificateFile.contentType)
+
+            LOGGER.error(message)
+            throw IllegalArgumentException(message)
+        }
     }
 }
